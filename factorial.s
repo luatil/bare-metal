1ec67bb37f6cfd85ab24207bec8b2dbb98abd0c6
Luã <luasantilli@gmail.com>
2021-02-24 15:53:37 -0300

    Add comments to factorial.s

--- factorial.s
+++ factorial.s
@@ -1,16 +1,16 @@
 global _start
 
 _start:
-    mov ebp, 4
-    mov eax, ebp
-    dec eax
-    call fact
-    mov eax, 1
-    int 0x80
+    mov ebp, 4          ; Move 4 to the ebp registerj
+    mov eax, ebp        ; Move the contents of ebp to the eax register
+    dec eax             ; Decrement eax by 1
+    call fact           
+    mov eax, 1          ; Set the sysexit system call on eax register
+    int 0x80            ; Perform system call
 fact: 
-    imul ebp, eax
-    dec eax
-    cmp eax, 1
-    jg fact
-    mov ebx, ebp
-    ret
+    imul ebp, eax       ; Multiply the contents of ebp register by eax
+    dec eax             ; decrement the eax register by one
+    cmp eax, 1          ; Compare the eax register to one
+    jg fact             ; If eax is greater than one, jump to fact
+    mov ebx, ebp        ; Else move the contents from ebp register to ebx 
+    ret                 ; Return to after the call to fact
